1b841e2c4fe290c06b3a8071384e8d856fa1cd69
wyattferguson <wyattf@gmail.com>
2026-06-09 16:54:53 -0400

    New build with proper cartridge header.

--- make.bat
+++ make.bat
@@ -9,4 +9,4 @@ lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o ./build/logo_data.o ./src/sprites/l
 lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o ./build/logo_map.o ./src/sprites/logo_map.c
 lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o ./build/trophy_data.o ./src/sprites/trophy_data.c
 lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o ./build/trophy_map.o ./src/sprites/trophy_map.c
-lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -o ./2048.gb ./build/main.o ./build/board.o ./build/font.o ./build/screens.o ./build/utils.o ./build/block_tiles.o ./build/number_tiles.o ./build/logo_data.o ./build/logo_map.o ./build/trophy_data.o ./build/trophy_map.o
+lcc -Z -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -Wm-yoA -Wm-yn"2048" -o ./2048.gb ./build/main.o ./build/board.o ./build/font.o ./build/screens.o ./build/utils.o ./build/block_tiles.o ./build/number_tiles.o ./build/logo_data.o ./build/logo_map.o ./build/trophy_data.o ./build/trophy_map.o
--- makefile
+++ makefile
@@ -4,20 +4,20 @@ CC = lcc
 
 CFLAGS = -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf--debug -c
 
-LDFLAGS = -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf--debug
+LDFLAGS = -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf--debug -Z -Wm-yoA -Wm-yn"2048"
 
-SRC = src/main.c src/board.c src/font.c src/screens.c src/utils.c
+SRC = src/main.c src/board.c src/font.c src/screens.c src/utils.c src/sprites/block_tiles.c src/sprites/number_tiles.c src/sprites/logo_data.c src/sprites/logo_map.c src/sprites/trophy_data.c src/sprites/trophy_map.c
 
 OBJ = $(patsubst src/%.c, $(BUILD_DIR)/%.o, $(SRC))
 
 BUILD_DIR = build
 
 $(BUILD_DIR)/%.o: src/%.c
-
+	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) -o $@ $<
 
 all: $(BUILD_DIR)/$(PROJECTNAME).gb
 
-$(PROJECTNAME).gb: $(OBJ)
+$(BUILD_DIR)/$(PROJECTNAME).gb: $(OBJ)
 
 	$(CC) $(LDFLAGS) -o $@ $(OBJ)
